691e6d029bee9b443199993975f51dfdf1e3725d
thanhhkhoi <khoinguyen2003hg@gmail.com>
2023-06-09 22:43:23 +0700

    menu

---
+++ Assets/Scripts/DieuKhienMenuLV.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DieuKhienMenuLV : MonoBehaviour
+{
+    public void PlayGame1()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void PlayGame3()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
